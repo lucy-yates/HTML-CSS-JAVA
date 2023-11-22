31b5f7a1e14ea43eac7a93d64d9851d2c29c6899
unknown <lucy.yates@ntlworld.com>
2023-11-22 12:10:10 +0000

    day three week three

--- starter.js
+++ starter.js
@@ -1,4 +1,4 @@
-console.log ("hello from starter")
+console.log("hello from starter")
 let myname = "lucy"
 console.log(myname);
 console.log(typeof myname);
@@ -29,7 +29,7 @@ console.log(4 / 4);
 console.log(4 ** 4);
 console.log(10 % 4);
 
-let numm=12;
+let numm = 12;
 console.log(numm == 24);
 console.log('1' == 1);
 
@@ -53,12 +53,12 @@ console.log((6 + 10) / 2 * 5 - 10);
 // 2
 console.log(x = x + 1);
 // 3
-console.log(x+= 1);
+console.log(x += 1);
 // Try and figure out the difference
 // 3
 console.log(x++);
 // 5
- console.log(++x);
+console.log(++x);
 
 // Relational Operators
 // true
@@ -66,7 +66,7 @@ console.log(5 > 3);
 // false
 console.log(3 != 3);
 // false
-console.log(!5>3);
+console.log(!5 > 3);
 
 // Mismatched Types
 // 55
@@ -78,11 +78,11 @@ console.log(5 * "5");
 // true
 console.log(1 == true);
 //false
- console.log(1 === true);
+console.log(1 === true);
 
- console.log("==================");
+console.log("==================");
 
-const arr = ["lucy","beth", "jayne", "tony"];
+const arr = ["loosie", "buff", "janyewest", "bigtone"];
 console.log(arr);
 console.log(arr[0]);
 console.log(arr[1]);
@@ -94,11 +94,11 @@ let age = 10
 console.log(age);
 
 if (age <= 17) {
-                console.log("underage");
+    console.log("underage");
 
- } else if (age <= 65)  {
-                console.log("insurable");
-            
+} else if (age <= 65) {
+    console.log("insurable");
+
 
 } else {
     console.log("out of range");
@@ -106,72 +106,164 @@ if (age <= 17) {
 
 console.log("==================");
 let crunch = 1;
-for (let crunch=1; crunch <=100; crunch++) {
+for (let crunch = 1; crunch <= 100; crunch++) {
     console.log(crunch);
-    if (crunch % 3 ==0 && crunch %5 ==0 && crunch%2==0) {
+    if (crunch % 3 == 0 && crunch % 5 == 0 && crunch % 2 == 0) {
         console.log("fizz buzz wham");
-        }
-    
-        else if (crunch % 3 == 0 && crunch%2==0) {
-            console.log("fizz wham");
-        }
-        
-        else if (crunch % 5 == 0 && crunch%2==0) {
-            console.log("buzz wham");
-        }
-       
-        else if (crunch % 2 == 0) {
-            console.log("wham");
-        }
-    
-        else if (crunch % 3 == 0) {
+    }
+
+    else if (crunch % 3 == 0 && crunch % 2 == 0) {
+        console.log("fizz wham");
+    }
+
+    else if (crunch % 5 == 0 && crunch % 2 == 0) {
+        console.log("buzz wham");
+    }
+
+    else if (crunch % 2 == 0) {
+        console.log("wham");
+    }
+
+    else if (crunch % 3 == 0) {
         console.log("fizz");
-        }
-    
-        else if (crunch % 5 == 0)  {
+    }
+
+    else if (crunch % 5 == 0) {
         console.log("buzz");
-        } 
-    
-        else {
+    }
+
+    else {
         console.log("george michael");
-        }
+    }
 }
 console.log("==================");
 
 let munch = 17
-if (munch % 3 ==0 && munch %5 ==0 && munch%2==0) {
+if (munch % 3 == 0 && munch % 5 == 0 && munch % 2 == 0) {
     console.log("fizz buzz wham");
-    }
+}
 
-    else if (munch % 3 == 0 && munch%2==0) {
-        console.log("fizz wham");
-    }
-    
-    else if (munch % 5 == 0 && munch%2==0) {
-        console.log("buzz wham");
-    }
-   
-    else if (munch % 2 == 0) {
-        console.log("wham");
-    }
+else if (munch % 3 == 0 && munch % 2 == 0) {
+    console.log("fizz wham");
+}
 
-    else if (munch % 3 == 0) {
+else if (munch % 5 == 0 && munch % 2 == 0) {
+    console.log("buzz wham");
+}
+
+else if (munch % 2 == 0) {
+    console.log("wham");
+}
+
+else if (munch % 3 == 0) {
     console.log("fizz");
-    }
+}
 
-    else if (munch % 5 == 0)  {
+else if (munch % 5 == 0) {
     console.log("buzz");
-    } 
+}
 
-    else {
+else {
     console.log("george michael");
-    }
+}
 
-    console.log("==================");
- 
+console.log("==================");
 
- 
- for (let k=1; k<=10; k++) {
+
+
+for (let k = 1; k <= 10; k++) {
     console.log("hello");
- }
- 
+}
+
+console.log("==================");
+
+for (let index = 0; index < 4; index++) {
+    const element = arr[index];
+    console.log(arr[index]);
+}
+
+console.log("==================");
+
+console.log("for-of-loop");
+for (let char of arr) {
+    console.log("char:", char);
+}
+
+console.log("==================");
+
+const multof2 = [2,4,6,8,10]
+for (let number of multof2) {
+    console.log("number:", number);
+}
+
+console.log("==================");
+
+var nums = [1,2,3,4,5,6,7,8,9,10], 
+    num = 0;
+for (var i = 0; i < nums.length; i++) {
+    num += +nums[i];
+    console.log(num);
+}
+
+
+console.log("==================");
+
+const generateCoinChange = (pence,cost) => {
+    let tenner = 0;
+    let fiver = 0;
+    let twopound = 0;
+    let onepound = 0;
+    let fiftypence = 0;
+    let twentypence = 0;
+    let tenpence = 0;
+    let fivepence = 0;
+    let twopence = 0;
+    let onepence = 0;
+    let remainingChange = pence-cost;
+
+    while (remainingChange >= 1000) {
+        remainingChange -= 1000;
+        tenner++;
+    }
+    while (remainingChange >= 500) {
+        remainingChange -= 500;
+        fiver++;
+    }
+    while (remainingChange >= 200) {
+        remainingChange -= 200;
+        twopound++;
+    }
+    while (remainingChange >= 100) {
+        remainingChange -= 100;
+        onepound++;
+    }
+    while (remainingChange >= 50) {
+        remainingChange -= 50;
+        fiftypence++;
+    }
+    while (remainingChange >= 20) {
+        remainingChange -= 20;
+        twentypence++;
+    }
+    while (remainingChange >= 10) {
+        remainingChange -= 10;
+        tenpence++;
+    }
+    while (remainingChange >= 5) {
+        remainingChange -= 5;
+        fivepence++;
+    }
+    while (remainingChange >= 2) {
+        remainingChange -= 2;
+        twopence++;
+    }
+    while (remainingChange > 0) {
+        remainingChange -= 1;
+        onepence++;
+    }
+    console.log(`£10: ${tenner}, £5: ${fiver}, £2: ${twopound}, £1: ${onepound}, 50p: ${fiftypence}, 20p: ${twentypence}, 10p: ${tenpence}, 5p: ${fivepence}, 2p: ${twopence}, 1p: ${onepence}`)
+};
+generateCoinChange(940, 500);
+generateCoinChange(1000, 379);
+generateCoinChange(230, 100);
+generateCoinChange(2000, 112);
